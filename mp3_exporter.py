13c36663f9d7929c87b946756808455e8951c97d
1234ab <nevenincs007@gmail.com>
2022-04-09 20:17:36 +0200

    remove hardcoded path, this still needs improvement

--- mp3_exporter.py
+++ mp3_exporter.py
@@ -4,7 +4,7 @@ import sys
 from nbswave import render_audio
 
 def export(in_file):
-    render_audio(in_file, in_file[:-4] + ".wav", default_sound_path="/home/balint/Documents/GitHub/nbs-audio/sounds")
+    render_audio(in_file, in_file[:-4] + ".wav", default_sound_path="/path/to/Sounds")
 
 if __name__ == '__main__':
     export(sys.argv[1])
